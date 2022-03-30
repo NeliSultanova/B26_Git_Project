6c5c4b40d209460ad66b0b6ba04c1d51751fb080
Neli Sultanova <nekasultanova@gmail.com>
2022-03-30 18:50:40 -0400

    updated again

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("java");
         System.out.println("working..");
+        System.out.println("git");
     }
 
     //extra 1 line
